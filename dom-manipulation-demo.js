f65763566ff31dc81fa752b77c5b8beb39af2838
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-03-16 12:30:32 +0100

    remove all done tasks

--- dom-manipulation-demo.js
+++ dom-manipulation-demo.js
@@ -1,6 +1,12 @@
 const taskFrom = document.getElementById('task-form');
 const taskNameInput = document.getElementById('task-name-input');
 const taskList = document.getElementById('task-list');
+const resetDoneTaskBtn = document.getElementById('reset-all-done-task-btn');
+
+function handleTaskClick(event) {
+  const taskElement = event.target;
+  taskElement.classList.toggle('task-done');
+}
 
 taskFrom.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -11,7 +17,10 @@ taskFrom.addEventListener('submit', function (event) {
   taskNameInput.value = '';
 });
 
-function handleTaskClick(event) {
-  const taskElement = event.target;
-  taskElement.classList.toggle('task-done');
-}
+resetDoneTaskBtn.addEventListener('click', function () {
+  const doneTasks = document.querySelectorAll('.task-done');
+  for (let i = 0; i < doneTasks.length; i += 1) {
+    const doneTask = doneTasks[i];
+    doneTask.remove();
+  }
+});
